a96103fcfc22b49ad00d21821788c5827319ff93
jamesvictorosu <102988423+jamesvictorosu@users.noreply.github.com>
2025-02-27 17:54:32 -0600

    Create jvm_password_generator.py
    adding pw generator program

---
+++ jvm_password_generator.py
@@ -0,0 +1,40 @@
+# JVM Password Generator program
+# This program generates a random password and performs exception handling
+import string
+import random
+
+# Ask user for desired password length
+while True:
+    try:
+        length = int(
+            input("Enter a number for password length (MIN 8, MAX 40): "))
+        if length <= 0:
+            raise ValueError("Length must be a positive number.")
+        if length < 8:
+            raise ValueError("Password length must be at least 8.")
+        if length > 40:
+            raise ValueError("Password length must not exceed 40.")
+        break
+    except ValueError as e:
+        print(f"Error: {e} Please retry.")
+
+# Define character sets
+all_characters = string.ascii_letters + string.digits + string.punctuation
+
+# Generate password at desired length with at least one of each character type for added security
+while True:
+    password = ''.join(random.choice(all_characters)
+                       for _ in range(length))
+
+ # Enhance password strength
+ # Check if the password contains at least one lowercase, one uppercase, one digit, and one special character
+ # any() is a built-in Python function that returns True if at least one element of the iterable is True, else returns False
+ # The while loop will keep running (regenerating the password) until the password meets all four conditions
+    if (any(c.islower() for c in password) and
+        any(c.isupper() for c in password) and
+        any(c.isdigit() for c in password) and
+            any(c in string.punctuation for c in password)):
+        break
+
+# Display generated password
+print(f"Generated password: {password}")
